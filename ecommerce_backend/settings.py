026eb9fd945e8817384b6a4a3911eede3d143659
iCodeCoolStuff <cakelord@protonmail.com>
2020-01-04 20:33:52 -0500

    add ez setup django on production

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'j!ppxh5ha=pb87x%u50-es*d6qw5x&7y^opp7jp)my&-jbka=6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False #True
@@ -144,3 +145,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())

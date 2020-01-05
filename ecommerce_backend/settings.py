950a899e6128c7d2ed3143599a1a414a57eb975f
iCodeCoolStuff <cakelord@protonmail.com>
2020-01-05 16:16:05 -0500

    add amazon s3

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'shop.apps.ShopConfig',
+    'storages'
 ]
 
 AUTH_USER_MODEL = 'shop.User'
@@ -158,5 +159,14 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())

cd01908ca8f1653e733172ec19e1c0dcffc5c35e
iCodeCoolStuff <cakelord@protonmail.com>
2020-01-06 18:09:22 -0500

    show order items in django admin

--- shop/admin.py
+++ shop/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import User, ImageSet, Order, Product
+from .models import User, ImageSet, Order, Product, OrderItem
 
 
 class CustomUserAdmin(UserAdmin):
@@ -25,7 +25,15 @@ class CustomUserAdmin(UserAdmin):
     ordering = ('email',)
 
 
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+class OrderAdmin(admin.ModelAdmin):
+    items = "order__items"
+    inlines = [OrderItemInline]
+
+
 admin.site.register(User, CustomUserAdmin)
 admin.site.register(ImageSet)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Product)

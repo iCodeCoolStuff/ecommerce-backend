1196ff0be54da92fbdeae6467cdf727ec0d5f4ba
iCodeCoolStuff <cakelord@protonmail.com>
2020-01-06 17:11:44 -0500

    add product import

--- shop/admin.py
+++ shop/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import User, ImageSet, Order
+from .models import User, ImageSet, Order, Product
 
 
 class CustomUserAdmin(UserAdmin):

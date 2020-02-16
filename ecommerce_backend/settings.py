91ca3b4be495b043297dd71ffa3b09c8047eae17
iCodeCoolStuff <cakelord@protonmail.com>
2020-02-15 20:41:11 -0500

    change jwt expiration to 30 mins

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+import datetime
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -100,6 +101,10 @@ REST_FRAMEWORK = {
         #'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
     'COERCE_DECIMAL_TO_STRING': False
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=30)
 }
 
 

b296c9e6bcc0daa080abe244cacfb2192e270fb3
iCodeCoolStuff <cakelord@protonmail.com>
2020-08-08 17:25:26 -0400

    update permissions config

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -104,8 +104,6 @@ REST_FRAMEWORK = {
         #'rest_framework.authentication.SessionAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES' : [
-        'shop.permissions.IsAdminOrWriteOnly',
-        'shop.permissions.UserPermission',
         'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ],
     'COERCE_DECIMAL_TO_STRING': False,
